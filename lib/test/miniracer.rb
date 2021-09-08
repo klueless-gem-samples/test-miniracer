34e59dc93c8f063af814c99e757a2ccf401e3296
David Cruwys <david@ideasmen.com.au>
2021-09-08 13:56:06 +1000

    first commit

---
+++ lib/test/miniracer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "miniracer/version"
+
+module Test
+  module Miniracer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/test/miniracer/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Test
+  module Miniracer
+    VERSION = "0.1.0"
+  end
+end
