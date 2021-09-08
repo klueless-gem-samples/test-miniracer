3156164731dec8ebd439324ff332529393053d93
David Cruwys <david@ideasmen.com.au>
2021-09-08 14:07:59 +1000

    initial setup

--- spec/test/miniracer_spec.rb
+++ spec/test/miniracer_spec.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe Test::Miniracer do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Test::Miniracer::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'has a standard error' do
+    expect { raise Test::Miniracer::Error, 'some message' }
+      .to raise_error('some message')
   end
 end
